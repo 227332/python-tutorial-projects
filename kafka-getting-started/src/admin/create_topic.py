4464c5ef8d83caa2f966e655f8a9659dbbba161e
Rosa S <17686382+227332@users.noreply.github.com>
2024-03-10 21:37:41 +0100

    [kafka-getting-started] Update topic creation code to raise exception in case of failure

--- kafka-getting-started/src/admin/create_topic.py
+++ kafka-getting-started/src/admin/create_topic.py
@@ -28,6 +28,42 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def topic_exists(admin_client: AdminClient, topic_name: str) -> bool:
+    metadata = admin_client.list_topics()
+    for t in iter(metadata.topics.values()):
+        if t.topic == topic_name:
+            return True
+    return False
+
+
+def create_topic(
+    admin_client: AdminClient, topic_name: str, num_partitions: int
+) -> None:
+    topic_list = [
+        NewTopic(topic=topic_name, num_partitions=num_partitions, replication_factor=1),
+    ]
+    try:
+        # asynchronously call to create topics
+        # By default this operation on the broker returns immediately while
+        # topics are deleted in the background, but here we give it some time (5s)
+        # to propagate in the cluster before returning
+        result_dict = admin_client.create_topics(topic_list, request_timeout=5.0)
+    except Exception as error:
+        print("Invalid operation")
+        raise error
+
+    for topic, future in result_dict.items():
+        try:
+            # You could pass a timeout here as well, but it is better to do it in the
+            # create_topics() method so that, in case of multiple topics, we wait for
+            # all futures at the same time
+            future.result()  # The result itself is None
+            print(f"Successfully created topic {topic}")
+        except Exception as error:
+            print(f"Failed to create topic {topic}: {error=}")
+            raise error
+
+
 def main() -> None:
     args = parse_args()
     topic_name = args.topic
@@ -43,14 +79,11 @@ def main() -> None:
         }
     )
 
-    topic_list = [
-        NewTopic(topic=topic_name, num_partitions=num_partitions, replication_factor=1),
-    ]
-    try:
-        admin_client.create_topics(topic_list)
-        print(f"Successfully created topic {topic_name}")
-    except Exception as err:
-        raise err
+    if not topic_exists(admin_client, topic_name):
+        print(f"Topic {topic_name} does not exist. Creating it...")
+        create_topic(admin_client, topic_name, num_partitions)
+    else:
+        print(f"Topic {topic_name} already exists, so it cannot be created again.")
 
 
 if __name__ == "__main__":
